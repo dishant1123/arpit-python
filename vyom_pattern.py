cc592711739bec93c14118f697625ff5f427892f
dishant1123 <dishantshah1417@gmail.com>
2025-10-03 17:40:21 +0530

    ....

---
+++ vyom_pattern.py
@@ -0,0 +1,129 @@
+"""
+* * * * *  *           * * * * * 
+* * * * *  * *         * * * *
+* * * * *  * * *       * * * 
+* * * * *  * * * *     * *
+* * * * *  * * * * *   *
+"""
+
+"""for i in range(1,6):
+    for j in range(1,6):
+        print("*",end=" ")
+    print()
+"""
+#2 :    
+"""
+for i in range(1,6):  # 2  
+    for j in range(1,i+1): # 1,3  
+        print("*",end=" ")  #  * 
+    print()                  # * *
+"""
+
+#3 :
+"""
+for i in range(6,1,-1):  # 2  
+    for j in range(1,i): # 1,3  
+        print("*",end=" ")  #  * 
+    print()                  # * *
+    
+"""
+
+"""
+1
+12
+123
+1234
+12345
+"""
+"""
+for i in range(1,6):  #2 ,6
+    for j in range(1,i+1): # 2,3   
+        print(i,end=" ")  # 1   
+    print()               # 2 2 
+"""
+
+"""
+a 
+a b 
+a b c
+a b c d
+a b c d e
+"""
+"""
+for i in range(1,6):  # 2 ,6  
+    ch=97
+    for j in range(i): #  0 1  
+        print(chr((ch+j)),end=" ")  #   a  
+    print()                  #          a b 
+"""
+
+"""
+5.          6.               7.          8.       
+* * * * *    * * * * *        *         *
+  * * * *     * * * *       * *        * * 
+    * * *      * * *      * * *       * * *
+      * *       * *     * * * *      * * * * 
+        *        *    * * * * *     * * * * * 
+"""
+
+# 5 : 
+"""
+for i in range(1,6):  # 2  
+    for k in range(1,i):  # 1,2 
+        print(" ",end=" ")  
+    for j in range(5,i-1,-1): # 5 1   
+        print("*",end=" ")    # * * * * *   
+    print()                   #   * * * *
+"""
+
+# 6 :
+"""
+for i in range(1,6):  # 2  
+    for k in range(1,i):  # 1,2 
+        print(" ",end="")  
+    for j in range(5,i-1,-1): # 5 1   
+        print("*",end=" ")    # * * * * *   
+    print()                   #   * * * *
+"""
+
+# 7 :
+"""for i in range(1,6):  #  
+    for k in range(5,i,-1):
+        print(" ",end=" ")
+    for j in range(1,i+1): # 1,3  
+        print("*",end=" ")  #  * 
+    print()                  # * *
+""" 
+
+# 8 :
+"""for i in range(1,6):  #  
+    for k in range(5,i,-1):
+        print(" ",end="")
+    for j in range(1,i+1): # 1,3  
+        print("*",end=" ")  #  * 
+    print()                  # * *
+"""
+
+# 9 :
+"""for i in range(1,5):  #  
+    for k in range(5,i,-1):
+        print(" ",end="")
+    for j in range(1,i+1): # 1,3  
+        print("*",end=" ")  #  * 
+    print()                  # * *
+for i in range(1,6):  # 2  
+    for k in range(1,i):  # 1,2 
+        print(" ",end="")  
+    for j in range(5,i-1,-1): # 5 1   
+        print("*",end=" ")    # * * * * *   
+    print()                   #   * * * *
+
+"""
+
+for i in range(1,5):  # 2  
+    for j in range(1,i+1):
+        if i % 2==0:  
+            print("#",end=" ")
+        else :
+            print("*",end=" ")   
+    print()                  

b224e0331756091c3453109c7d7af2a6bc27dbc7
dishant1123 <dishantshah1417@gmail.com>
2026-03-07 14:38:03 +0530

    ....

--- cpp/6march.cpp
+++ cpp/6march.cpp
@@ -54,7 +54,72 @@ int main()
 
 // ex :2 multiple inheritance
 /*
+
 class a 
 class b 
 class c : public a, public b
+
 */
+
+// ex :3 multi level inheritance
+/*
+class a 
+class b : public a 
+class c: public b
+*/
+
+#include <iostream>
+using namespace std;
+class employees 
+{
+    private : 
+        string  name; 
+    protected : 
+        int  age; 
+    public : 
+        employees(string n, int a)
+        {
+            name =n; 
+            age =a; 
+        }
+    void show()
+    {
+        cout<<"name : "<<name<<"\n";
+    }
+};
+class manager : public employees 
+{
+    public : 
+        string department; 
+    manager(string n, int a, string d):employees(n,a) // called base class constructor
+    {
+        department = d;
+    }
+};
+class senior_manager : public manager
+{
+    private : 
+        int salary; 
+    public : 
+        senior_manager(string n , int a, string d, int s):manager(n,a,d)
+        {
+            salary = s;
+        }
+    void display()
+    {
+        show(); 
+        cout<<"age is  : "<<age<<"\n";
+        cout<<"department is : "<<department<<"\n";
+        cout<<"salary is : "<<salary<<"\n";
+    }
+};
+int main()
+{
+    senior_manager s("arpit",23,"computer",10000); 
+    s.display(); 
+
+    manager m("vyom",19,"R & D");
+    m.show();
+    cout<<"department : "<<m.department<<"\n";
+    return 0;
+}
